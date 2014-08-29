16dc16ad0f4844cdee3442fb7d7b115f1f1b754d
Catherine Tran <tranc489@gmail.com>
2014-08-29 15:37:07 -0700

    add Competitor, Division, and Game classes to lib files. Update spec_helper.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,18 @@
+require 'active_record'
+require 'rspec'
+require 'pg'
+require 'shoulda-matchers'
+
+require 'division'
+require 'competitor'
+require 'game'
+
+Active::Base.establish_connection(YAML::load(File.open('./db/config.yml'))['test'])
+
+RSpec.configure do | config |
+  config.after(:each) do
+    Division.all.each { |division| division.destroy }
+    Competitor.all.each { |competitor| competitor.destroy}
+    Game.all.each { |game| game.destroy }
+  end
+end

2a659468f91b8892c25e38a723e7b4edc384b200
Catherine Tran <tranc489@gmail.com>
2014-08-29 16:18:11 -0700

    create name presence validation within competitor class.

--- lib/competitor.rb
+++ lib/competitor.rb
@@ -1,3 +1,5 @@
 class Competitor < ActiveRecord::Base
   belongs_to :division
+
+  validates :name, :presence => true
 end
--- spec/competitor_spec.rb
+++ spec/competitor_spec.rb
@@ -2,4 +2,9 @@ require 'spec_helper'
 
 describe Competitor do
   it { should belong_to :division}
+
+  it 'validates the presence of a name' do
+    competitor = Competitor.new ({:name => ''})
+    expect(competitor.save).to eq false
+  end
 end

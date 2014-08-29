ea46e8fe25636d247e973abca1deb3cf0762c77c
Catherine Tran <tranc489@gmail.com>
2014-08-29 16:15:37 -0700

    create name presence within division class.

--- lib/division.rb
+++ lib/division.rb
@@ -1,3 +1,5 @@
 class Division < ActiveRecord::Base
   has_many :competitors
+
+  validates :name, :presence => true
 end
--- spec/division_spec.rb
+++ spec/division_spec.rb
@@ -2,4 +2,9 @@ require 'spec_helper'
 
 describe Division do
   it { should have_many :competitors}
+
+  it 'validates the presence of a name' do
+    division = Division.new ({:name => ''})
+    expect(division.save).to eq false
+  end
 end

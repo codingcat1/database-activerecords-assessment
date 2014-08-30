05feb19f8b3b7336ce26185882f68bfe6ca951f3
Catherine Tran <tranc489@gmail.com>
2014-08-30 15:27:11 -0700

    update main menu in UI.

--- evo_tournament_ui.rb
+++ evo_tournament_ui.rb
@@ -1,36 +1,51 @@
+require 'active_record'
+
 require './lib/competitor'
 require './lib/division'
 require './lib/game'
 
 ActiveRecord::Base.establish_connection(YAML::load(File.open('./db/config.yml'))['development'])
 
-def welcome
-  system 'clear'
-  puts "*** WELCOME TO THE DATABASE FOR EVO ***"
-  menu
-end
 
 def menu
   choice = nil
   until choice == 'x'
-    puts "Press (1) to enter the Divisions menu"
-    puts "Press (2) to enter the Competitors menu"
-    puts "Press (3) to enter the Games menu"
-    puts "Press (4) to EXIT"
+    system 'clear'
+    puts "*** WELCOME TO THE DATABASE FOR EVO ***\n\n"
+    puts "*** DIVISIONS MENU ***"
+    puts "Press (1) to add a Division"
+    puts "Press (2) to view existing Divisions\n\n"
+    puts "*** COMPETITORS MENU ***"
+    puts "Press (3) to add a Competitor"
+    puts "Press (4) to view all Competitors\n\n"
+    puts "*** GAMES MENU ***"
+    puts "Press (5) to add a Game"
+    puts "Press (6) to view all Games\n\n"
+    puts "Press (x) to EXIT"
     choice = gets.chomp
     case choice
     when '1'
-      divisions_menu
+      add_division
     when '2'
-      competitors_menu
+      view_divisions
     when '3'
-      games_menu
+      add_competitor
+    when '4'
+      view_competitors
+    when '5'
+      add_game
+    when '6'
+      view_games
     when 'x'
       puts "OK BYE"
       sleep(1)
       system 'clear'
     else
       puts 'Sorry, not a valid entry'
+      sleep(1)
     end
   end
 end
+
+
+menu

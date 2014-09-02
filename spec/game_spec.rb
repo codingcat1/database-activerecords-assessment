f720eb167c8f7c3da844e5783c30a1e683146742
Catherine Tran <tranc489@gmail.com>
2014-09-02 15:53:57 -0700

    pass spec to show association of competitors within games.

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe Game do
+  it { should have_many :competitions }
+  it { should have_many :competitors }
+
   it 'validates the presence of a name' do
     game = Game.new ({:name => ''})
     expect(game.save).to eq false

d4fdc073b56e9496339c221c9e28ed5fc35e5300
Catherine Tran <tranc489@gmail.com>
2014-09-02 15:51:24 -0700

    pass spec to show association of games within competitors.

--- spec/competitor_spec.rb
+++ spec/competitor_spec.rb
@@ -3,6 +3,9 @@ require 'spec_helper'
 describe Competitor do
   it { should belong_to :division}
 
+  it { should have_many :competitions }
+  it { should have_many :games }
+
   it 'validates the presence of a name' do
     competitor = Competitor.new ({:name => ''})
     expect(competitor.save).to eq false
